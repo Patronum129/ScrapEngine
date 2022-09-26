9d474efd5dc24ee68d5844c4e7cc56fa1bd62410
Patronum129 <diego88rising@gmail.com>
2022-09-26 15:05:05 +0200

    restart

--- src/Test.cpp
+++
@@ -1,9 +0,0 @@
-#include "Test.h"
-#include "stdio.h"
-
-namespace ScrapEngine {
-
-	void Print() {
-		printf("Welcome to ScrapEngine World!\n");
-	}
-}
--- src/Test.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace ScrapEngine {
-
-	__declspec(dllexport) void Print();
-	
-}
